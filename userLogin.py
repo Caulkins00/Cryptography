daccac327b3676fe03c4def195434a81b414f5e3
willcaulkins <caulwg18@wfu.edu>
2023-04-04 22:49:38 -0400

    Integrate SQLite functionality

--- userLogin.py
+++ userLogin.py
@@ -3,28 +3,28 @@ import base64
 import os
 import hashlib
 import json
-import sqlite3
+import sqlite3 as sql
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-userDB = 'userDB.json'
-file = open(userDB)
-users = json.load(file)
-file.close()
+con = sql.connect("pwdManager.db")
+cur = con.cursor()
+
+# res = cur.execute("SELECT * FROM users")
+# print(res.fetchall())
 
 def is_valid_credentials(username, password):
-    for entry in users:
-        if entry['username']==username:
-            password_hash = hashlib.sha256(password+bytes(entry['salt'],'utf-8')).hexdigest()
-            if password_hash == entry['password_hash']:
-                print('Success!')
-                return True
-            else:
-                print('Incorrect username of password... Try again!')
-                return False
-    print('Incorrect username or password... Try again!')
-    return False
+
+    res = cur.execute("SELECT * FROM users WHERE username =?", (username,))
+    user = res.fetchone()
+    password_hash = hashlib.sha256(password+bytes(user[2],'utf-8')).hexdigest()
+    if password_hash == user[1]:
+        print('Success!')
+        return True
+    else:
+        print('Incorrect username or password... Try again!')
+        return False
 
 def prompt_login():
     username = input("Username: ")
@@ -37,36 +37,25 @@ def add_user():
 
     username = input("What would you like your username to be? ")
 
-    for entry in users:
-        if entry['username']==username:
-            print("This username is taken by another user. Please pick a new one.")
-            add_user()
+    count = cur.execute("SELECT COUNT(username) FROM users WHERE username =?", (username,)).fetchone()[0]
+    table_count = cur.execute("SELECT COUNT(username) FROM users").fetchone()[0]
+    if count>0 and table_count>0:
+        print("This username is taken by another user. Please pick a new one.")
+        add_user()
     password = getpass.getpass("What would you like your password to be? ").encode()
     password_hash = hashlib.sha256(password+bytes(salt,'utf-8')).hexdigest()
+    cur.execute("INSERT INTO users VALUES (?,?,?)", (username,password_hash,salt,))
+    con.commit()
+    print('User added!')
 
-    write_json(username, password_hash, salt)
-
-def remove_user(username, password):
-    for entry in users:
-        if entry['username']==username:
-            if entry['password_hash'] == hashlib.sha256(password + bytes(entry['salt'],'utf-8')).hexdigest():
-                users.remove(entry)
-                print('User removed.\n')
-            else:
-                print('Incorrect password')
-                password = getpass.getpass('Enter teh password of the user you would like to remove: ')
-                remove_user(username, password)
-    write_json()
-
-
-def write_json(*args):
-    file = open(userDB,'w')
-    if len(args)==3:
-        dic = dict(username = args[0], password_hash = args[1], salt = args[2])
-        users.append(dic)
-    outfile = json.dumps(users, indent=4)
-    file.write(outfile)
-    file.close()
+def remove_user(username):
+    password = getpass.getpass('Enter the password of the user you would like to remove: ').encode()
+    if is_valid_credentials(username, password):
+        cur.execute("DELETE FROM users WHERE username = ?", (username, ))
+        con.commit()
+        print('User deleted!')
+    else:
+        remove_user(username)
 
 def main():
     prompt = input("What would you like to do? (login (l), add user(a), remove user(r), quit(q)) ")
@@ -78,13 +67,12 @@ def main():
         case 'a':
             add_user()
             main()
-        case 'q':
-            quit()
         case 'r':
             username = input('Enter the username of the user you would like to remove: ')
-            password = getpass.getpass('Enter the password of the user you would like to remove: ').encode()
-            remove_user(username, password)
+            remove_user(username)
             main()
+        case 'q':
+            quit()
         case _:
             print('Input not recognized. Please try again.')
             main()
