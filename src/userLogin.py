117f5ea964f9342ca968acf57c51ba5ae955bb48
willcaulkins <will.caulkins@gmail.com>
2023-04-10 16:44:57 -0400

    clean up

--- src/userLogin.py
+++ src/userLogin.py
@@ -4,25 +4,17 @@ import os
 import hashlib
 import json
 import sqlite3 as sql
-# from cryptography.fernet import Fernet
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 con = sql.connect("pwdManager.db")
 cur = con.cursor()
-
-# res = cur.execute("SELECT * FROM users")
-# print(res.fetchall())
 
 def is_valid_credentials(username, password):
     res = cur.execute("SELECT * FROM users WHERE username =?", (username,))
     user = res.fetchone()
     password_hash = hashlib.sha256(password+bytes(user[2],'utf-8')).hexdigest()
     if password_hash == user[1]:
-        # print('Success!')
         return True
     else:
-        # print('Incorrect username or password... Try again!')
         return False
 
 def prompt_login():
@@ -48,20 +40,9 @@ def add_user():
     print('User added!')
 
 def add_user_gui():
-    salt = os.urandom(16).hex()
-
-    username = input("What would you like your username to be? ")
-
-    count = cur.execute("SELECT COUNT(username) FROM users WHERE username =?", (username,)).fetchone()[0]
-    table_count = cur.execute("SELECT COUNT(username) FROM users").fetchone()[0]
-    if count>0 and table_count>0:
-        print("This username is taken by another user. Please pick a new one.")
-        add_user()
-    password = getpass.getpass("What would you like your password to be? ").encode()
-    password_hash = hashlib.sha256(password+bytes(salt,'utf-8')).hexdigest()
-    cur.execute("INSERT INTO users VALUES (?,?,?)", (username,password_hash,salt,))
-    con.commit()
-    print('User added!')
+    
+    # this needs to be basically the same as the above, but utilizing the gui interface instead of cmd line
+    pass
 
 def remove_user(username):
     password = getpass.getpass('Enter the password of the user you would like to remove: ').encode()
